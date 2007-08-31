8f55e59bb7656ae50599099392e7972833d650a4
tball <devnull@localhost>
2007-08-31 21:46:06 +0000

    Simple command-line scripts for *nix and Windows.  The *nix script has bootclasspath support for OS X, but hasn't been tested.

---
+++ bin/javafxc.bat
@@ -0,0 +1,33 @@
+@echo off
+
+REM JavaFX Script compiler script for Linux/Solaris/OS X.
+REM
+REM Uses the same arguments as the JDK's javac command.
+
+REM %~dp0 is expanded pathname of the current script
+set _JAVAFXC_HOME=%~dp0..
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java.exe
+goto runCompiler
+
+:noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=java.exe
+
+set _CMD_LINE_ARGS=%1
+if ""%1""=="""" goto runCompiler
+shift
+:setupArgs
+if ""%1""=="""" goto runCompiler
+set _CMD_LINE_ARGS=%_CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+
+:runCompiler
+"%_JAVACMD%" -jar "%_JAVAFXC_HOME%\javafx.jar" %_CMD_LINE_ARGS%
+
+REM cleanup
+if not "%_JAVAFXC_HOME"=="" set _JAVAFXC_HOME=
+if not "%_JAVACMD%"=="" set _JAVACMD=
+if not "%_CMD_LINE_ARGS%"=="" set ANT_CMD_LINE_ARGS=
