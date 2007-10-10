bee662944cafd7ccf62c873708a2fbf5c86542ec
briangoetz <devnull@localhost>
2007-10-10 17:42:57 +0000

    JFXC-62: add copyright notices everywhere; correct dates on existing copyright notices.

--- src/share/classes/com/sun/tools/javafx/tree/JFXAbstractTriggerOn.java
+++ src/share/classes/com/sun/tools/javafx/tree/JFXAbstractTriggerOn.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2006 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 1999-2007 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,7 +29,7 @@ import com.sun.tools.javac.tree.JCTree.JCBlock;
 
 /**
  * trigger on ...
- */;
+ */
 public abstract class JFXAbstractTriggerOn extends JFXStatement {
     public JCBlock block;
 
