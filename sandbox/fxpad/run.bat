e95f3bf78dcbb5d5cfb9955150e3d3f0ff2e3758
chrisoliver <devnull@localhost>
2007-11-19 17:34:07 +0000

    fixing shell script

--- sandbox/fxpad/run.bat
+++ sandbox/fxpad/run.bat
@@ -5,6 +5,6 @@ REM  Copyright 2007 Sun Microsystems, Inc. All rights reserved.
 REM  SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
 REM
 
-set classpath=%~dp0..;%~dp0lib/jfxFormat.jar;
-rem %~dp0lib/javac.jar;%~dp0lib/javafxc.jar
-%~dp0..\..\bin\javafx.bat javafxcpad.Main
+set classpath="%~dp0;%~dp0javafxcpad/lib/jfxFormat.jar;%~dp0lib/javafxrt.jar;%~dp0lib/Filters.jar;%~dp0lib/swing-layout.jar;%~dp0lib/script.jar"
+java -classpath %classpath% net.java.javafx.FXShell javafxcpad.Main
+
