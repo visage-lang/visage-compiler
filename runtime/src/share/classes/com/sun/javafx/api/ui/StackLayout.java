575cc83402da083fa57c1aec8f35eaad0049cad4
jimclarke <devnull@localhost>
2007-11-26 02:39:12 +0000

    Initial port from interpreter

---
+++ runtime/src/share/classes/com/sun/javafx/api/ui/StackLayout.java
@@ -0,0 +1,135 @@
+/* 
+ * Copyright 2007 Sun Microsystems, Inc.  All Rights Reserved. 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER. 
+ * 
+ * This code is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License version 2 only, as 
+ * published by the Free Software Foundation.  Sun designates this 
+ * particular file as subject to the "Classpath" exception as provided 
+ * by Sun in the LICENSE file that accompanied this code. 
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * version 2 for more details (a copy is included in the LICENSE file that 
+ * accompanied this code). 
+ * 
+ * You should have received a copy of the GNU General Public License version 
+ * 2 along with this work; if not, write to the Free Software Foundation, 
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ * 
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara, 
+ * CA 95054 USA or visit www.sun.com if you need additional information or 
+ * have any questions. 
+ */  
+
+package com.sun.javafx.api.ui;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.LayoutManager2;
+import java.awt.Rectangle;
+import java.util.LinkedList;
+import java.util.List;
+
+public class StackLayout implements LayoutManager2 {
+    public static final String BOTTOM = "bottom";
+    public static final String TOP = "top";
+    
+    private List<Component> components = new LinkedList<Component>();
+
+    public void addLayoutComponent(Component comp, Object constraints) {
+        synchronized (comp.getTreeLock()) {
+            if (BOTTOM.equals(constraints)) {
+                components.add(0, comp);
+            } else if (TOP.equals(constraints)) {
+                components.add(comp);
+            } else {
+                components.add(comp);
+            }
+        }
+    }
+    
+    public void addLayoutComponent(String name, Component comp) {
+        addLayoutComponent(comp, TOP);
+    }
+
+    public void removeLayoutComponent(Component comp) {
+        synchronized (comp.getTreeLock()) {
+            components.remove(comp);
+        }
+    }
+    
+    public float getLayoutAlignmentX(Container target) {
+        return 0.5f;
+    }
+
+    public float getLayoutAlignmentY(Container target) {
+        return 0.5f;
+    }
+
+    public void invalidateLayout(Container target) {
+    }
+
+    public Dimension preferredLayoutSize(Container parent) {
+        synchronized (parent.getTreeLock()) {
+            int width = 0;
+            int height = 0;
+
+            for (Component comp: components) {
+                Dimension size = comp.getPreferredSize();
+                width = Math.max(size.width, width);
+                height = Math.max(size.height, height);
+            }
+
+            Insets insets = parent.getInsets();
+            width += insets.left + insets.right;
+            height += insets.top + insets.bottom;
+            
+            return new Dimension(width, height);
+        }
+    }
+
+    public Dimension minimumLayoutSize(Container parent) {
+        synchronized (parent.getTreeLock()) {
+            int width = 0;
+            int height = 0;
+
+            for (Component comp: components) {
+                Dimension size = comp.getMinimumSize();
+                width = Math.max(size.width, width);
+                height = Math.max(size.height, height);
+            }
+
+            Insets insets = parent.getInsets();
+            width += insets.left + insets.right;
+            height += insets.top + insets.bottom;
+            
+            return new Dimension(width, height);
+        }
+    }
+    
+    public Dimension maximumLayoutSize(Container target) {
+        return new Dimension(Integer.MAX_VALUE,
+                             Integer.MAX_VALUE);
+    }
+
+    public void layoutContainer(Container parent) {
+        synchronized (parent.getTreeLock()) {
+            int width = parent.getWidth();
+            int height = parent.getHeight();
+            
+            Rectangle bounds = new Rectangle(0, 0, width, height);
+
+            int componentsCount = components.size();
+            
+            for (int i = 0; i < componentsCount; i++) {
+                Component comp = components.get(i);
+                comp.setBounds(bounds);
+                parent.setComponentZOrder(comp, componentsCount - i - 1);
+            }
+        }
+    }
+}
