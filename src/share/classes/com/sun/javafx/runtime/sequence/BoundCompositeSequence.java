b5e042ef9cf4ccaa161a51e505dc0b84ba0d9480
bothner <devnull@localhost>
2007-12-07 05:09:00 +0000

    Ooops - prevous check-in caused a test-suite failure I didn't notice. Revert it until I can debug it.

--- src/share/classes/com/sun/javafx/runtime/sequence/BoundCompositeSequence.java
+++ src/share/classes/com/sun/javafx/runtime/sequence/BoundCompositeSequence.java
@@ -8,14 +8,10 @@ import com.sun.javafx.runtime.location.SequenceLocation;
  * BoundCompositeSequence
  *
  * @author Brian Goetz
- * @author Per Bothner
  */
 public class BoundCompositeSequence<T> extends AbstractBoundSequence<T> implements SequenceLocation<T> {
     private final SequenceLocation<? extends T>[] locations;
     private final int[] startPositions;
-    private final Sequence<? extends T>[] sequences;
-    private int size, depth;
-
 
     public static<T> SequenceLocation<T> make(Class<T> clazz, SequenceLocation<? extends T>... locations) {
         return new BoundCompositeSequence<T>(clazz, false, locations);
@@ -25,13 +21,11 @@ public class BoundCompositeSequence<T> extends AbstractBoundSequence<T> implemen
         super(clazz, false, lazy);
         this.locations = locations;
         this.startPositions = new int[locations.length];
-        sequences = new Sequence[locations.length];
     }
 
     protected void computeInitial() {
-        int depth = 0;
-        int offset = 0;
-        for (int i = 0; i < locations.length; i++) {
+        Sequence<? extends T>[] sequences = Util.newSequenceArray(locations.length);
+        for (int i = 0, offset = 0; i < locations.length; i++) {
             locations[i].addChangeListener(new MyListener(i));
             sequences[i] = locations[i].get();
             Class eClass = locations[i].get().getElementType();
@@ -39,44 +33,12 @@ public class BoundCompositeSequence<T> extends AbstractBoundSequence<T> implemen
                 throw new ClassCastException("cannot cast "+eClass.getName()
                                              +" segment to "+clazz.getName()+" sequence");
             startPositions[i] = offset;
-            int size = sequences[i].size();
-            offset += size;
-            depth = Math.max(depth, sequences[i].getDepth());
+            offset += sequences[i].size();
         }
-        this.size = offset;
-        this.depth = depth + 1;
-        value = new MyCompositeSequence(clazz);
+        value = Sequences.concatenate(clazz, sequences);
         setValid(true);
     }
 
-    class MyCompositeSequence extends AbstractSequence<T> implements Sequence<T> {
-        public MyCompositeSequence(Class<T> clazz) {
-            super(clazz);
-        }
- 
-        @Override
-        public int size() {
-            return size;
-        }
-
-        @Override
-        public int getDepth() {
-            return depth;
-        }
-
-        @Override
-        public T get(int position) {
-            if (position < 0 || position >= size || size == 0) {
-                throw new IndexOutOfBoundsException(Integer.toString(position));
-            }
-            // Linear search should be good enough for now
-            int chunk = 0;
-            while (chunk < sequences.length - 1 && (position >= startPositions[chunk + 1] || sequences[chunk].size() == 0)) {
-                ++chunk;
-            }
-            return sequences[chunk].get(position - startPositions[chunk]);
-        }
-    }
 
     private class MyListener implements SequenceChangeListener<T> {
         private final int index;
@@ -87,17 +49,16 @@ public class BoundCompositeSequence<T> extends AbstractBoundSequence<T> implemen
 
         public void onInsert(int position, T element) {
             int actualPos = startPositions[index] + position;
-            sequences[index] = locations[index].get();
+            value = value.insertBefore(element, actualPos);
             for (int i=index+1; i<startPositions.length; i++)
                 ++startPositions[i];
-            size++;
             valueChanged();
             BoundCompositeSequence.this.onInsert(actualPos, element);
         }
 
         public void onDelete(int position, T element) {
             int actualPos = startPositions[index] + position;
-            sequences[index] = locations[index].get();
+            value = value.delete(actualPos);
             for (int i=index+1; i<startPositions.length; i++)
                 --startPositions[i];
             valueChanged();
@@ -107,7 +68,7 @@ public class BoundCompositeSequence<T> extends AbstractBoundSequence<T> implemen
         public void onReplace(int position, T oldValue, T newValue) {
             int actualPos = startPositions[index] + position;
             T old = value.get(actualPos);
-            sequences[index] = locations[index].get();
+            value = value.set(actualPos, newValue);
             valueChanged();
             BoundCompositeSequence.this.onReplaceElement(actualPos, old, newValue);
         }
