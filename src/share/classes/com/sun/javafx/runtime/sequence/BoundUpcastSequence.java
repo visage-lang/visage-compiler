da23cbe91099d917d7b0eba7b373103a506799e2
briangoetz <devnull@localhost>
2008-03-19 22:02:42 +0000

    More efficient implementation

--- src/share/classes/com/sun/javafx/runtime/sequence/BoundUpcastSequence.java
+++ src/share/classes/com/sun/javafx/runtime/sequence/BoundUpcastSequence.java
@@ -11,12 +11,10 @@ import com.sun.javafx.runtime.location.SequenceLocation;
 public class BoundUpcastSequence<T, V extends T> extends AbstractBoundSequence<T> {
 
     private final SequenceLocation<V> sequence;
-    private final Class<V> vClazz;
 
     public BoundUpcastSequence(Class<T> clazz, SequenceLocation<V> sequence) {
         super(clazz);
         this.sequence = sequence;
-        this.vClazz = sequence.get().getElementType();
     }
 
     protected Sequence<T> computeValue() {
@@ -26,7 +24,7 @@ public class BoundUpcastSequence<T, V extends T> extends AbstractBoundSequence<T
     protected void initialize() {
         sequence.addChangeListener(new SequenceChangeListener<V>() {
             public void onChange(int startPos, int endPos, Sequence<? extends V> newElements, Sequence<V> oldValue, Sequence<V> newValue) {
-                updateSlice(startPos, endPos, Sequences.upcast(vClazz, newElements));
+                updateSlice(startPos, endPos, newElements, Sequences.upcast(getClazz(), newValue));
             }
         });
     }
