fc6e5b01ae2c05fa930a73c1414e8631989a42c1
tball <devnull@localhost>
2008-04-27 17:50:22 +0000

    Fixed javafxpad for Mac OS X, Java 5 use.

--- demos/javafxpad/debug.sh
+++ demos/javafxpad/debug.sh
@@ -1,3 +1,3 @@
-JFX=/export/home/jclarke/src/openjfx-compiler
+JFX=../../openjfx-compiler
 JFX_LIB=$JFX/dist/lib
-java -jar -agentlib:jdwp=transport=dt_socket,address=4444,server=y,suspend=y dist/JavaFXPad.jar
+java -jar -Xbootclasspath/p:dist/lib/javafxc.jar:dist/lib/javafxrt.jar:dist/lib/Scenario.jar -agentlib:jdwp=transport=dt_socket,address=4444,server=y,suspend=y dist/JavaFXPad.jar
