e705dc74d6caabde9ce93b57e3015fe93c544bf6
briangoetz <devnull@localhost>
2008-05-16 20:33:10 +0000

    Fix lexer bugs in intellij plugin.

--- sandbox/intellij-plugin/src/com/sun/javafx/ideaplugin/FxLexer.java
+++ sandbox/intellij-plugin/src/com/sun/javafx/ideaplugin/FxLexer.java
@@ -9,6 +9,7 @@ import com.sun.tools.javac.util.Context;
 import com.sun.tools.javafx.antlr.v3Lexer;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.Token;
+import org.antlr.runtime.RecognitionException;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -28,7 +29,6 @@ public class FxLexer extends LexerBase {
     }
 
     public void start(final CharSequence buffer, int startOffset, int endOffset, final int initialState) {
-        System.out.printf("reparsing from %d to %d%n", startOffset, endOffset);
         this.buffer = buffer;
         bufferStart = startOffset;
         bufferEnd = endOffset;
@@ -42,31 +42,31 @@ public class FxLexer extends LexerBase {
 
     @Nullable
     public IElementType getTokenType() {
-        System.out.printf("Returning %d:%s @ %d:%d%n", nextToken.getType(), FxTokens.getElement(nextToken.getType()), getTokenStart(), getTokenEnd());
         if (nextToken.getType() == v3Lexer.EOF)
             return null;
         IElementType result = FxTokens.getElement(nextToken.getType());
         if (result == null) {
-            System.out.println("unknown token type");
+            System.out.printf("unknown token type %d%n", nextToken.getType());
             return TokenType.BAD_CHARACTER;
         }
         return result;
     }
 
     public int getTokenStart() {
-        return curStart;
+        return bufferStart + curStart;
     }
 
     public int getTokenEnd() {
-        return curEnd;
+        return bufferStart + curEnd;
     }
 
     public void advance() {
-        System.out.println("advancing from " + lexer.getCharIndex());
         curStart = lexer.getCharIndex();
         nextToken = lexer.nextToken();
         curEnd = lexer.getCharIndex();
-        System.out.printf("Processed %d:%s @ %d:%d%n", nextToken.getType(), FxTokens.getElement(nextToken.getType()), getTokenStart(), getTokenEnd());
+        System.out.printf("Processed %d:%s @ %d:%d/%d%n", nextToken.getType(), FxTokens.getElement(nextToken.getType()), getTokenStart(), getTokenEnd(), bufferEnd);
+        if (curEnd == curStart && nextToken.getType() != v3Lexer.EOF)
+            System.out.printf("Failed to advance position: %d:%d%n", curStart, curEnd);
     }
 
     @Deprecated
