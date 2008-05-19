b6b18f08b4a910ae8eae72796a40e26871600bb0
briangoetz <devnull@localhost>
2008-05-19 21:39:18 +0000

    Add brace matching support.

--- sandbox/intellij-plugin/src/com/sun/javafx/ideaplugin/FxLanguage.java
+++ sandbox/intellij-plugin/src/com/sun/javafx/ideaplugin/FxLanguage.java
@@ -26,10 +26,14 @@
 package com.sun.javafx.ideaplugin;
 
 import com.intellij.lang.Language;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.lang.BracePair;
 import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * FxLanguage
@@ -44,5 +48,24 @@ public class FxLanguage extends Language {
     @NotNull
     public SyntaxHighlighter getSyntaxHighlighter(Project project, final VirtualFile virtualFile) {
         return new FxHighlighter();
+    }
+
+    @Nullable
+    public PairedBraceMatcher getPairedBraceMatcher() {
+        return new PairedBraceMatcher() { 
+            private final BracePair[] PAIRS = new BracePair[] {
+                    new BracePair('(',FxTokens.LPAREN.elementType, ')', FxTokens.RPAREN.elementType, false),
+                    new BracePair('[',FxTokens.LBRACKET.elementType, ']', FxTokens.RBRACKET.elementType, false),
+                    new BracePair('{',FxTokens.LBRACE.elementType, '}', FxTokens.RBRACE.elementType, true)
+            };
+
+            public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType iElementType, @Nullable IElementType iElementType1) {
+                return true;
+            }
+
+            public BracePair[] getPairs() {
+                return PAIRS;
+            }
+        };
     }
 }
