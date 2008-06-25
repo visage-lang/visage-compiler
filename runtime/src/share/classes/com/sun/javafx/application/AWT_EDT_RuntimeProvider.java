d9b069e0f14b976f6213af75642f13c573d24b89
shan_man <devnull@localhost>
2008-06-25 01:49:01 +0000

    Merging javafxgui

---
+++ runtime/src/share/classes/com/sun/javafx/application/AWT_EDT_RuntimeProvider.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package com.sun.javafx.application;
+
+import java.lang.reflect.Method;
+import java.awt.EventQueue;
+import com.sun.javafx.runtime.RuntimeProvider;
+import com.sun.javafx.runtime.sequence.Sequences;
+import java.lang.reflect.InvocationTargetException;
+
+public class AWT_EDT_RuntimeProvider implements RuntimeProvider {
+
+    public boolean usesRuntimeLibrary(Class application) {
+        return true;
+    }
+
+    public Object run(final Method entryPoint, final String... args) throws Throwable  {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    InternalHelper.initDefaultLAF();
+                    entryPoint.invoke(null, Sequences.make(String.class, args));
+                } catch (IllegalAccessException ignore) {
+                    throw new AssertionError();
+                } catch (InvocationTargetException ite) {
+                    Throwable cause = ite.getCause();
+                    StackTraceElement[] stack = cause.getStackTrace();
+                    int n = 0;
+                    while (n < stack.length) {
+                        if (stack[n++].getMethodName().equals(entryPoint.getName())) {
+                            break;
+
+                        }
+                    }
+                    StackTraceElement[] shortStack = new StackTraceElement[n];
+                    System.arraycopy(stack, 0, shortStack, 0, n);
+                    cause.setStackTrace(shortStack);
+                    cause.printStackTrace();
+                }
+            }
+        });
+
+        return null;
+    }
+
+    public void deferTask(Runnable task) {
+        EventQueue.invokeLater(task);
+    }
+
+}
---
+++ runtime/src/share/classes/com/sun/javafx/scene/MediaHelper.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package com.sun.javafx.scene;
+
+import com.sun.media.jmc.*;
+import com.sun.media.jmc.control.VideoControl;
+
+/**
+ * 
+ */
+public class MediaHelper {
+    
+    // FX doesn't seem to like our generics, esp. w/
+    // classes or unitialized objects obj.getClass()
+    public static VideoControl getVideoControl(MediaProvider mp) {
+        if (mp == null) {
+            return null;
+        }
+        VideoControl vc;
+        vc = mp.getControl(VideoControl.class);
+        return vc;
+    }
+    
+    public static String getStringMetadata(Media mediaInfo, String key) {
+        return mediaInfo.getMetadata(key, String.class);
+    }
+
+}
