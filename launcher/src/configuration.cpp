371e6d805ed10e66115c46287d2309d8bdb16373
tball <devnull@localhost>
2008-06-26 21:06:43 +0000

    Updated default classpaths for Windows launchers.

--- launcher/src/configuration.cpp
+++ launcher/src/configuration.cpp
@@ -105,7 +105,7 @@ void Configuration::init() {
 
     // set default classpath-libraries for javafx
     if (javafx_classpath_libs.empty()) {
-        javafx_classpath_libs = "javafxrt.jar;Scenario.jar;Decora-HW.jar;Decora-D3D.jar;jmc.jar";
+        javafx_classpath_libs = "javafxrt.jar;javafxgui.jar;javafx-swing.jar;Scenario.jar;Decora-HW.jar;Decora-D3D.jar;jmc.jar";
     }
     
     // set default bootclass-libraries for javafxc
