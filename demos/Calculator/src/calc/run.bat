25a3f392946df73229e24ffdc93bcd18b72cf16d
shan_man <devnull@localhost>
2008-07-18 05:25:38 +0000

    Don't need swing-layout.

--- demos/Calculator/src/calc/run.bat
+++ demos/Calculator/src/calc/run.bat
@@ -1,2 +1,2 @@
 javac -g CalcJSR223.java
-java -classpath ".;..;../../javafxrt.jar;../../lib/swing-layout.jar;../../lib/Filters.jar" CalcJSR223
+java -classpath ".;..;../../javafxrt.jar;../../lib/Filters.jar" CalcJSR223
--- demos/Calculator/src/calc/run.sh
+++ demos/Calculator/src/calc/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #javac -g CalcJSR223.java
-javac -classpath "../../lib/script.jar:/../lib/swing-layout.jar:../../lib/Filters.jar" -g CalcJSR223.java
-java -classpath ".:..:../../javafxrt.jar:../../lib/script.jar:../../lib/swing-layout.jar:../../lib/Filters.jar" CalcJSR223
+javac -classpath "../../lib/script.jar:../../lib/Filters.jar" -g CalcJSR223.java
+java -classpath ".:..:../../javafxrt.jar:../../lib/script.jar:../../lib/Filters.jar" CalcJSR223
 
