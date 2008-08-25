c5dc997c314bbfc658dd4b94e84f6ead167c8245
freespirit <devnull@localhost>
2008-08-25 19:40:26 +0000

    Revert 3868->3869 as there is an implementation of the SPI hiding out there ...

--- src/share/classes/com/sun/javafx/runtime/Entry.java
+++ src/share/classes/com/sun/javafx/runtime/Entry.java
@@ -39,21 +39,36 @@ import java.security.PrivilegedAction;
  * @author Tom Ball
  */
 public class Entry {
+    private static RuntimeProvider provider;
 
     public static void start(Class<?> app, String[] commandLineArgs) throws Throwable {
         Method main = app.getMethod(entryMethodName(), Sequence.class);
         Object args = Sequences.make(String.class, commandLineArgs);
-
-        main.setAccessible(true);
+        
         try {
-            main.invoke(null, args);
-        } catch (InvocationTargetException e) {
-            throw e.getCause();
+            main.setAccessible(true);
+            provider = runtimeProviderLocator();
+            if (provider != null && provider.usesRuntimeLibrary(app)) {
+                provider.run(main, commandLineArgs);
+            } else {
+                try {
+                    main.invoke(null, args);
+                } catch (InvocationTargetException e) {
+                    throw e.getCause();
+                }
+            }
+        } catch (AccessControlException ex) {
+            // applet or jnlp app security problem:  try just running main
+             try {
+                main.invoke(null, args);
+            } catch (InvocationTargetException e) {
+                throw e.getCause();
+            }
         }
     }
 
     public static void deferTask(final Function0<Void> function) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
+        deferTask(new Runnable() {
             public void run() {
                 function.invoke();
             }
@@ -61,10 +76,95 @@ public class Entry {
     }
 
     public static void deferTask(Runnable function) {
-        java.awt.EventQueue.invokeLater(function);
+        if (provider == null)
+            provider = runtimeProviderLocator();
+        assert provider != null;
+        provider.deferTask(function);
     }
 
+    private static RuntimeProvider runtimeProviderLocator() {
+        Iterator<?> iterator;
+        Class<?> loaderClass;
+        String loadMethodName;
+        boolean usingServiceLoader;
+
+        try {
+            // Lookup Java 6 public API first
+            loaderClass = Class.forName("java.util.ServiceLoader");
+            loadMethodName = "load";
+            usingServiceLoader = true;
+        } catch (ClassNotFoundException cnfe) {
+                // Lookup Java 5 Sun-private API
+                final Class<?>[] cls = new Class<?>[1];
+                AccessController.doPrivileged(
+                    new PrivilegedAction<Void>() {
+                        public Void run() {
+                            try {
+                                cls[0] = Class.forName("sun.misc.Service");
+                            } catch (ClassNotFoundException cnfe2) {
+                                cls[0] = null;
+                            }
+                            return null;
+                        }
+                    });
+                loaderClass = cls[0];
+                loadMethodName = "providers";
+                usingServiceLoader = false;
+        }
+        if (loaderClass == null)
+            throw new AssertionError("Failed discovering ServiceLoader");
+
+        try {
+            // java.util.ServiceLoader.load or sun.misc.Service.providers
+            Method loadMethod = loaderClass.getMethod(loadMethodName,
+                    Class.class,
+                    ClassLoader.class);
+            ClassLoader cl = Entry.class.getClassLoader();
+            Object result = loadMethod.invoke(null, RuntimeProvider.class, cl);
+
+            // For java.util.ServiceLoader, we have to call another
+            // method to get the iterator.
+            if (usingServiceLoader) {
+                Method m = loaderClass.getMethod("iterator");
+                result = m.invoke(result); // serviceLoader.iterator();
+            }
+
+            iterator = (Iterator<?>) result;
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+        try {
+            return iterator.hasNext() ? (RuntimeProvider) iterator.next() : null;
+        } catch (Error e) {
+            // ServiceConfigurationError is in java.util in Java 6, sun.misc in Java 5,
+            // so ignore its package
+            if (e.getClass().getSimpleName().equals("ServiceConfigurationError"))
+                return new NoRuntimeDefault(); // no service found
+            else
+                throw e;
+        }
+    }
+    
     public static String entryMethodName() {
         return "javafx$run$";
+    }
+    
+    private static class NoRuntimeDefault implements RuntimeProvider {
+
+        public boolean usesRuntimeLibrary(Class application) {
+            return true;
+        }
+
+        public Object run(Method entryPoint, String... args) throws Throwable {
+            try {
+                return entryPoint.invoke(null, (Object)args);
+            } catch (InvocationTargetException e) {
+                throw e.getCause();
+            }
+        }
+
+        public void deferTask(Runnable task) {
+            java.awt.EventQueue.invokeLater(task);
+        }        
     }
 }
