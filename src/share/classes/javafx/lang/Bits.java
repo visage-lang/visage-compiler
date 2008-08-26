c1080a1fe302d2069b92634a686075b244d040eb
chrisoliver <devnull@localhost>
2008-08-26 16:04:40 +0000

    adding support for bit operations to library

---
+++ src/share/classes/javafx/lang/Bits.java
@@ -0,0 +1,90 @@
+package javafx.lang;
+
+/**
+ *  Bit operations
+ */
+
+public class Bits {
+
+    public static int shiftLeft(int value, int shift) {
+        return value << shift;
+    }
+
+    public static int shiftRight(int value, int shift) {
+        return value >> shift;
+    }
+
+    public static int bitOr(int value, int mask) {
+        return value | mask;
+    }
+
+    public static int bitAnd(int value, int mask) {
+        return value & mask;
+    }
+    
+    public static int bitXor(int value, int mask) {
+        return value ^ mask;
+    }
+
+    public static int complement(int value) {
+        return ~value;
+    }
+
+    public static int add(int value, int mask) {
+        return value | mask;
+    }
+
+    public static int remove(int value, int mask) {
+        return value & ~mask;
+    }
+
+    public static boolean contains(int value, int mask) {
+        return (value & mask) != 0;
+    }
+
+    public static int clear(int value, int mask) {
+        return value & ~mask;
+    }
+
+
+
+    public static long shiftLeft(long value, int shift) {
+        return value << shift;
+    }
+
+    public static long shiftRight(long value, int shift) {
+        return value >> shift;
+    }
+
+    public static long bitOr(long value, long mask) {
+        return value | mask;
+    }
+
+    public static long bitAnd(long value, long mask) {
+        return value & mask;
+    }
+    
+    public static long bitXor(long value, long mask) {
+        return value ^ mask;
+    }
+
+    public static long complement(long value) {
+        return ~value ;
+    }
+
+    public static long add(long value, long mask) {
+        return value | mask;
+    }
+
+    public static long remove(long value, long mask) {
+        return value & ~mask;
+    }
+
+    public static boolean contains(long value, long mask) {
+        return (value & mask) != 0;
+    }
+
+    public static long clear(long value, long mask) {
+        return value & ~mask;
+    }
+}
