8c0272ee874a29f26e8d3347dbc4892f58666586
briangoetz <devnull@localhost>
2008-09-10 13:46:03 +0000

    Create directory for performance test helpers; create test helper for exercising sequences

---
+++ test/perf/com/sun/javafx/runtime/sequences/SequenceExerciser.java
@@ -0,0 +1,117 @@
+package com.sun.javafx.runtime.sequences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.javafx.runtime.location.SequenceLocation;
+import com.sun.javafx.runtime.location.SequenceVariable;
+import com.sun.javafx.runtime.sequence.Sequences;
+
+/**
+ * SequenceExerciser
+ *
+ * @author Brian Goetz
+ */
+public class SequenceExerciser {
+    private static Map<String, Class<? extends SequenceOpGenerator>> ops = new HashMap<String, Class<? extends SequenceOpGenerator>>();
+    static int random = (int) System.nanoTime();
+    static {
+        ops.put("rr", RandomReadGenerator.class);
+        ops.put("rw", RandomWriteGenerator.class);
+        ops.put("ra", RandomAccessGenerator.class);
+    }
+
+    public static int xorShift(int x) {
+        x ^= (x << 6);
+        x ^= (x >>> 21);
+        x ^= (x << 7);
+        return x;
+    }
+
+    public static int nextRandom(int bound) {
+        int ret = random % bound;
+        random = xorShift(random);
+        return ret;
+    }
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+        int initialSize = Integer.parseInt(args[0]);
+        SequenceLocation<Integer> seq = SequenceVariable.make(Sequences.range(1, initialSize));
+        int sum = 0;
+        for (int i=1; i<args.length; i++) {
+            String[] parts = args[1].split(" ");
+            Class<? extends SequenceOpGenerator> g = ops.get(parts[0]);
+            SequenceOpGenerator gg = g.newInstance();
+            gg.parseOptions(parts, 1);
+            sum += gg.doOp(seq);
+        }
+    }
+}
+
+interface SequenceOpGenerator {
+    public void parseOptions(String[] options, int startingPoint);
+    public int doOp(SequenceLocation<Integer> seq);
+}
+
+class RandomReadGenerator implements SequenceOpGenerator {
+    private int opCount;
+
+    public void parseOptions(String[] options, int start) {
+        opCount = Integer.parseInt(options[start]);
+    }
+
+    public int doOp(SequenceLocation<Integer> seq) {
+        int size = seq.get().size();
+        int sum = 0;
+        for (int i=0; i<opCount; i++)
+            sum += seq.get(SequenceExerciser.nextRandom(size));
+        return sum;
+    }
+}
+
+class RandomWriteGenerator implements SequenceOpGenerator {
+    private int opCount;
+
+    public void parseOptions(String[] options, int start) {
+        opCount = Integer.parseInt(options[start]);
+    }
+
+    public int doOp(SequenceLocation<Integer> seq) {
+        int size = seq.get().size();
+        int sum = 0;
+        for (int i=0; i<opCount; i++) {
+            int value = SequenceExerciser.nextRandom(1000);
+            sum += value;
+            seq.set(SequenceExerciser.nextRandom(size), value);
+        }
+        return sum;
+    }
+}
+
+class RandomAccessGenerator implements SequenceOpGenerator {
+    private int opCount;
+    private int readPercentage;
+    
+    public void parseOptions(String[] options, int start) {
+        opCount = Integer.parseInt(options[start]);
+        readPercentage = Integer.parseInt(options[start+1]);
+    }
+
+    public int doOp(SequenceLocation<Integer> seq) {
+        int size = seq.get().size();
+        int sum = 0;
+        for (int i=0; i<opCount; i++) {
+            int key = SequenceExerciser.nextRandom(100);
+            if (key < readPercentage) {
+                sum += seq.get(SequenceExerciser.nextRandom(size));
+            }
+            else {
+                int value = SequenceExerciser.nextRandom(1000);
+                sum += value;
+                seq.set(SequenceExerciser.nextRandom(size), value);
+            }
+        }
+        return sum;
+    }
+}
+
