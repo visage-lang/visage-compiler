ef6bf193f54ec30c5ca92d942b6c34364dac8c77
rfield <devnull@localhost>
2008-12-11 03:12:03 +0000

    Fixed JFXC-2481 : Support for bound comorehension of new types    var fs : Float[];    var y = bind for (xi in fs) xi;

---
+++ src/share/classes/com/sun/javafx/runtime/sequence/LongBoundComprehension.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package com.sun.javafx.runtime.sequence;
+
+import com.sun.javafx.runtime.location.*;
+import com.sun.javafx.runtime.TypeInfo;
+
+/**
+ * Support for a for-loop with a Number induction variable.
+ * 
+ * @author Brian Goetz
+ * @author Robert Field
+ */
+public abstract class LongBoundComprehension<V> extends AbstractBoundComprehension<Long, LongLocation, V> {
+
+    public LongBoundComprehension(TypeInfo<V> typeInfo,
+                              SequenceLocation<Long> sequenceLocation,
+                              boolean useIndex) {
+        super(typeInfo, sequenceLocation, useIndex);
+    }
+
+    protected LongLocation makeInductionLocation(Long value) {
+        return LongVariable.make(value);
+    }
+}
