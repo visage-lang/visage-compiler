aa7c4104857bdfe7be378a39054935b5b721abdc
ksrini <devnull@localhost>
2009-03-18 13:16:22 -0700

    Fixed: JFXC-2921 : add javafxw launcher Summary:  * added javafxw launcher executable and associated sources for windows  * added additional tests for this variant of the launcher Reviewed-by: Sundar

--- launcher/nbproject/Makefile-javafxdoc_release.mk
+++ launcher/nbproject/Makefile-javafxdoc_release.mk
@@ -1,5 +1,5 @@
 #
-# Gererated Makefile - do not edit!
+# Generated Makefile - do not edit!
 #
 # Edit the Makefile in the project folder instead (../Makefile). Each target
 # has a -pre and a -post target defined where you can add customized code.
@@ -12,22 +12,26 @@ MKDIR=mkdir
 CP=cp
 CCADMIN=CCadmin
 RANLIB=ranlib
-CC=gcc.exe
-CCC=g++.exe
-CXX=g++.exe
+CC=gcc.exe.lnk
+CCC=g++.exe.lnk
+CXX=g++.exe.lnk
 FC=
+
+# Macros
+PLATFORM=Cygwin-Windows
 
 # Include project Makefile
 include Makefile
 
 # Object Directory
-OBJECTDIR=build/javafxdoc_release/Cygwin-Windows
+OBJECTDIR=build/javafxdoc_release/${PLATFORM}
 
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/src/configuration.o \
 	${OBJECTDIR}/src/javafxc.o \
 	${OBJECTDIR}/src/util.o \
+	${OBJECTDIR}/src/javafxw.o \
 	${OBJECTDIR}/src/javafxdoc.o \
 	${OBJECTDIR}/src/javafx.o
 
@@ -45,7 +49,8 @@ FFLAGS=
 LDLIBSOPTIONS=
 
 # Build Targets
-.build-conf: ${BUILD_SUBPROJECTS} dist/Release/javafxdoc.exe
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-javafxdoc_release.mk dist/Release/javafxdoc.exe
 
 dist/Release/javafxdoc.exe: ${OBJECTFILES}
 	${MKDIR} -p dist/Release
@@ -53,23 +58,33 @@ dist/Release/javafxdoc.exe: ${OBJECTFILES}
 
 ${OBJECTDIR}/src/configuration.o: src/configuration.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -o ${OBJECTDIR}/src/configuration.o src/configuration.cpp
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/configuration.o src/configuration.cpp
 
 ${OBJECTDIR}/src/javafxc.o: src/javafxc.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -o ${OBJECTDIR}/src/javafxc.o src/javafxc.cpp
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/javafxc.o src/javafxc.cpp
 
 ${OBJECTDIR}/src/util.o: src/util.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -o ${OBJECTDIR}/src/util.o src/util.cpp
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/util.o src/util.cpp
+
+${OBJECTDIR}/src/javafxw.o: src/javafxw.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/javafxw.o src/javafxw.cpp
 
 ${OBJECTDIR}/src/javafxdoc.o: src/javafxdoc.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -o ${OBJECTDIR}/src/javafxdoc.o src/javafxdoc.cpp
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/javafxdoc.o src/javafxdoc.cpp
 
 ${OBJECTDIR}/src/javafx.o: src/javafx.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src
-	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -o ${OBJECTDIR}/src/javafx.o src/javafx.cpp
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -DPROJECT_JAVAFXDOC -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/javafx.o src/javafx.cpp
 
 # Subprojects
 .build-subprojects:
@@ -81,3 +96,8 @@ ${OBJECTDIR}/src/javafx.o: src/javafx.cpp
 
 # Subprojects
 .clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ launcher/src/javafxw.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+#ifdef PROJECT_JAVAFXW
+
+#include "configuration.h"
+#include "util.h"
+#include <stdlib.h>
+#include <windows.h>
+#include <string>
+
+/*
+ * The only difference between this and javafx is that it uses teh WinMain
+ * entry point which allows us to suppress the MS-DOS Window when launched
+ * from cmd.exe or by the Windows OS.
+ */
+
+char **__initenv;
+
+int WINAPI WinMain(HINSTANCE inst, HINSTANCE previnst, LPSTR cmdline, int cmdshow) {
+    __initenv = _environ;
+
+    int     margc = __argc;
+    char**  margv = __argv;
+
+    Configuration config("execute_");
+    Util util;
+    int error;
+
+    if ( (error =  config.initConfiguration(margc, margv)) != (EXIT_SUCCESS) )  {
+        return error;
+    }
+
+    // construct command
+    std::string cmd = "\"" + config.javacmd + "\" ";
+    if (! config.vmargs.empty()) {
+        cmd += config.vmargs + " ";
+    }
+    if (! config.profile_nativelibpath.empty()) {
+        cmd += "-Djava.library.path=\"" + util.evaluatePath(config.javafxpath, config.profile_nativelibpath) + "\" ";
+    }
+    if (! config.profile_bootclasspath_prepend.empty()) {
+        cmd += "\"-Xbootclasspath/p:" + util.evaluatePath(config.javafxpath, config.profile_bootclasspath_prepend) + "\" ";
+    }
+    if (! config.profile_bootclasspath_append.empty()) {
+        cmd += "\"-Xbootclasspath/a:" + util.evaluatePath(config.javafxpath, config.profile_bootclasspath_append) + "\" ";
+    }
+    if (! config.profile_bootclasspath.empty()) {
+        cmd += "\"-Xbootclasspath:" + util.evaluatePath(config.javafxpath, config.profile_bootclasspath) + "\" ";
+    }
+    if (! config.profile_classpath.empty()) {
+        cmd += "-classpath \"" + util.evaluatePath(config.javafxpath, config.profile_classpath);
+        if (! config.classpath.empty()) {
+            cmd += ";" + config.classpath;
+        }
+        cmd += "\" ";
+    } else if (! config.classpath.empty()) {
+        cmd += "-classpath \"" + config.classpath + "\" ";
+    }
+
+    cmd += "com.sun.javafx.runtime.Main ";
+    cmd += config.fxargs;
+
+
+    return util.createProcess(cmd);
+}
+
+#endif  /* PROJECT_JAVAFXW */
+
+
