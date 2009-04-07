a77cec38a4b135c7a4a6091561defe958b683765
rfield <rfield@kenai.com>
2009-04-06 22:24:14 -0700

    Fix JFXC-3010 : Regression: else of if is evaluated even if the if expression is true the underlying cause was that lazy binding of element access wasn't making the initial index evaluation lazy. added regression tests for both the reported problem and the underlying problem

--- src/share/classes/com/sun/javafx/runtime/sequence/BoundSequenceElement.java
+++ src/share/classes/com/sun/javafx/runtime/sequence/BoundSequenceElement.java
@@ -40,7 +40,7 @@ class BoundSequenceElement<T> extends ObjectVariable<T> implements ObjectLocatio
         super();
         this.seq = seq;
         this.index = index;
-        lastIndex = index.get();
+        lastIndex = lazy? Integer.MAX_VALUE : index.get();
         bind(lazy, new AbstractBindingExpression() {
             public void compute() {
                 lastIndex = BoundSequenceElement.this.index.get();
@@ -51,6 +51,7 @@ class BoundSequenceElement<T> extends ObjectVariable<T> implements ObjectLocatio
     }
 
     private class MySequenceListener extends ChangeListener<T> {
+        @Override
         public void onChange(int startPos, int endPos, Sequence<? extends T> newElements, Sequence<T> oldValue, Sequence<T> newValue) {
             int deltaSize = (endPos-startPos+1) - Sequences.size(newElements);
             if (deltaSize != 0) {
