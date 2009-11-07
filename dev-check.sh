4bfd0e7455483c2b5621fffc801abd683b2f4a94
jjh <devnull@localhost>
2009-11-06 19:33:25 -0800

    Speed up test targets.  rm build/test before running, and speed up KeyFrame tests

--- dev-check.sh
+++ dev-check.sh
@@ -22,6 +22,8 @@ fi
 target=`cat build/test/dev-target`
 echo "-- target run was: $target"
 
+find test -name \*.fx > ./build/test/allTests
+
 # Note these filenames only have the last of the dirs between the test and test/, and they contain
 # / instead of \
 #passes=`fgrep '<td>Success' build/test/reports/junit-noframes.html  | sed -e 's@<td>@@' -e 's@<.*@@' | fgrep /`
@@ -29,7 +31,33 @@ echo "-- target run was: $target"
 
 # This includes successes from junit which have no / in their paths.  These are not filenames, but 
 # names of tests inside a file.
-passes=`fgrep '<td>Success' build/test/reports/junit-noframes.html  | sed -e 's@<td>@@' -e 's@<.*@@'`
+
+passingFiles=`fgrep '<td>Success' build/test/reports/junit-noframes.html  | sed -e 's@<td>@@' -e 's@<.*@@' | fgrep /`
+passingTests=`fgrep '<td>Success' build/test/reports/junit-noframes.html  | sed -e 's@<td>@@' -e 's@<.*@@' | fgrep -v /`
+
+# Expand filenames into full test/....xxx.fx form
+passes=
+if [ ! -z "$passingFiles" ] ; then
+    jjxx="$passingFiles"
+    for ii in $jjxx ; do
+        case $ii in
+            # a little opt here so we don't have to search all the regress's
+            regress* | should-fail* | currently-failing*)
+              passes="$passes test/$ii"
+              ;;
+            *)
+              full=`fgrep $ii ./build/test/allTests`
+              if [ $? = 0 ] ; then 
+                  # found it
+                 passes="$passes $full"
+             else
+                 echo "--Error: didn't find $ii in build/test/allTests"
+                 exit 1
+                 #passes="$passes $ii"
+             fi
+        esac
+    done
+fi
 
 # all tests run should have passed
 # This fail list DOES contain the whole test/... prefix
@@ -52,6 +80,21 @@ runtimeFails1=`grep '<td.*Output written' build/test/reports/junit-noframes.html
     -e 's@\.fx.*@.fx@' \
     -e 's@\\\@/@g'`
 
+# put full paths into runtimeFails1   
+if [ ! -z "$runtimeFails1" ] ; then
+    jjxx="$runtimeFails1"
+    runtimeFails1=
+    for ii in $jjxx ; do
+        full=`fgrep $ii ./build/test/allTests`
+        if [ $? = 0 ] ; then
+            # found it
+            runtimeFails1="$runtimeFails1 $full"
+        else
+            runtimeFails1="$runtimeFails1 $ii"
+        fi
+    done
+fi
+
 # Here is another form of failure -  we don't handle this one:
 #<tr valign="top" class="Error">
 #<td>testMethodOverloadInstance</td><td>Failure</td><td>null expected:&lt;[jj]&gt; but was:&lt;[kk]&gt;<code>
@@ -59,18 +102,6 @@ runtimeFails1=`grep '<td.*Output written' build/test/reports/junit-noframes.html
 #<br>junit.framework.ComparisonFailure: null expected:<[jj]> but was:<[kk]><br/>	at MethodOverload$TesterInstance.testInstanceRetInt(MethodOverload.fx:175)<br/>	at MethodOverload.testMethodOverloadInstance(MethodOverload.fx:336)<br/>	at framework.FXUnitTestWrapper.runTest(FXUnitTestWrapper.java:86)<br/></code></td><td>0.000</td>
 #</tr>
 # The test is MethodOverload.fx but it contains several subtests.  One of them can fail, but that doesn't cause MethodOverload itself to fail.
-
-
-# put full paths into runtimeFails1   
-if [ ! -z "$runtimeFails1" ] ; then
-    find test -name \*.fx > ./build/test/allTests
-    jjxx="$runtimeFails1"
-    runtimeFails1=
-    for ii in $jjxx ; do
-        full=`fgrep $ii ./build/test/allTests`
-        runtimeFails1="$runtimeFails1 $full"
-    done
-fi
 
       echo "$compilationFails" | sed -e 's@^ @@' -e 's@ $@@' | sed -e 's@ @\
 @g' | sort > ./build/test/dev-compilation-fails
@@ -87,8 +118,8 @@ fi
      nFails=`cat ./build/test/dev-actual-fails | wc -l`
 
      # count passes
-     if [ ! -z "$passes" ] ; then
-         echo $passes | sed -e 's@ @\
+     if [ ! -z "$passes" -o ! -z "$passingTests" ] ; then
+         echo $passes $passingTests  | sed -e 's@ @\
 @g' | sort > ./build/test/dev-passes
          nPasses=`cat ./build/test/dev-passes | wc -l`
      else
@@ -112,16 +143,13 @@ fi
          # An unexpected pass is on the pass list and on the actual fail list
          rm -f ./build/test/dev-unexpected-passes
          touch ./build/test/dev-unexpected-passes
-         rm -rf jj
-         touch jj
-         for ii in $passes ; do
-echo $ii >> jj
-            grep "^$ii\$" ./build/test/dev-expected-fails1 > /dev/null 2>&1
-            if [ $? = 0 ] ; then
-                grep "^$ii\$" ./build/test/allTests >> ./build/test/dev-unexpected-passes
-            fi
-         done
 
+         for ii in `cat ./build/test/dev-expected-fails1` ; do
+             echo "$passes" | fgrep " $ii" > /dev/null 2>&1
+             if [ $? = 0 ] ; then
+                 echo $ii >> ./build/test/dev-unexpected-passes
+             fi
+         done
          nUnexpectedPasses=`cat ./build/test/dev-unexpected-passes | wc -l`
          echo "-- $nPasses total passes, $nUnexpectedPasses unexpected passes:"
          cat ./build/test/dev-unexpected-passes | sed -e 's@^@ @'
