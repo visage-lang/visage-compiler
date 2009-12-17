b22cbbb0f7a2534acd930bfede34c11fed317461
sundararajana <devnull@localhost>
2009-12-17 19:43:45 +0530

    Changed -J-javaagent with -javaagent.

--- buildtools/fxbtrace/bin/fxbtrace.bat
+++ buildtools/fxbtrace/bin/fxbtrace.bat
@@ -10,7 +10,7 @@ if "%JAVAFX_HOME%"=="" goto noJavafxHome
 
 if not exist "%BTRACE_HOME%\build\btrace-agent.jar" goto noBTraceHome
 
-%JAVAFX_HOME%/bin/javafx -J-javaagent:%BTRACE_HOME%/build/btrace-agent.jar=dumpClasses=false,debug=false,unsafe=false,probeDescPath=.,noServer=true,script=%1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVAFX_HOME%/bin/javafx -javaagent:%BTRACE_HOME%/build/btrace-agent.jar=dumpClasses=false,debug=false,unsafe=false,probeDescPath=.,noServer=true,script=%1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :noBTraceHome
