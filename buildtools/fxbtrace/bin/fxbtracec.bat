5afb6a4a106d31fd40030938ffb9496d354d6e15
sundararajana <devnull@localhost>
2010-01-04 09:06:55 +0530

    Fixed fxbtracec.bat and fxbtrace.bat to avoid '/' (and therefor cygwin dependency). Now using '\'. Also, simplified "fxbtrace" and "fxbtrace" scripts for Unix.

--- buildtools/fxbtrace/bin/fxbtracec.bat
+++ buildtools/fxbtrace/bin/fxbtracec.bat
@@ -1,16 +1,10 @@
 @echo off
 
-rem %~dp0 is expanded pathname of the current script under NT
-set DEFAULT_BTRACE_HOME=%~dp0..
-
-if "%BTRACE_HOME%"=="" set BTRACE_HOME=%DEFAULT_BTRACE_HOME%
-set DEFAULT_BTRACE_HOME=
-
 if "%JAVAFX_HOME%"=="" goto noJavafxHome
 
 if not exist "%BTRACE_HOME%\build\btrace-agent.jar" goto noBTraceHome
 
-javac -cp "%BTRACE_HOME%/build/btrace-client.jar;%JAVAFX_HOME%/lib/shared/javafxrt.jar" %*
+javac -cp "%BTRACE_HOME%\build\btrace-client.jar;%JAVAFX_HOME%\lib\shared\javafxrt.jar" %*
 goto end
 
 :noBTraceHome
