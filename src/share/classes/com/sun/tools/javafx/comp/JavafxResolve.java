96da63a7efef37f9f316e1e2306ec721ba3f9ab2
mcimadamore <devnull@localhost>
2010-01-15 18:41:05 +0000

    Fixed JFXC-3975 : Compiled-bind: protected access error should be detected during attribution Description: JavaFX accessibility rules for protected differ from Java accessibility rules - bring them in sync

--- src/share/classes/com/sun/tools/javafx/comp/JavafxResolve.java
+++ src/share/classes/com/sun/tools/javafx/comp/JavafxResolve.java
@@ -303,7 +303,7 @@ public class JavafxResolve {
                      // In JLS 2e 6.6.2.1, the subclass restriction applies
                      // only to instance fields and methods -- types are excluded
                      // regardless of whether they are declared 'static' or not.
-                     ((sym.flags() & STATIC) != 0 || sym.kind == TYP || types.isSubtype(c.type, site))))
+                     ((sym.flags() & STATIC) != 0 || sym.kind == TYP || site.tsym.isSubClass(c, types))))
                 c = c.owner.enclClass();
             return c != null;
         }
