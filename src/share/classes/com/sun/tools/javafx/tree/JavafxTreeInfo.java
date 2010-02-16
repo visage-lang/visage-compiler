f903b4e6a9e4ddb6da872ade1cb03cc261598054
jimi <jimi@JIMI-PC>
2010-02-16 09:45:31 -0800

    Fixed: JFXC-3917
    The bug owner now has all the information required to find the symbols and source code positions of On Replace triggers.
    
    This change merely adds extra support for ON_REPLACE tags in to JavafxTreeInfo's symbolFor() method.
    
    As the objects returned from getFirstIndex() and getLastIndex() will return VAR_DEF as their tags, then they should already be resolvable via symbolFor(). However, I have added support for VAR_DEF to the the getStartpos() and getEndpos() mehtods of JavafxTreeInfo, which should make it easier to get this information from things that return VariableTree as their object type.

--- src/share/classes/com/sun/tools/javafx/tree/JavafxTreeInfo.java
+++ src/share/classes/com/sun/tools/javafx/tree/JavafxTreeInfo.java
@@ -477,7 +477,8 @@ public class JavafxTreeInfo {
             return symbolFor(((JFXFunctionInvocation) node).meth);
         case TOPLEVEL:
             return ((JFXScript) node).packge;
-
+        case ON_REPLACE:
+                return symbolFor(((JFXOnReplace) node).getOldValue());
         default:
             return null;
         }
@@ -539,6 +540,12 @@ public class JavafxTreeInfo {
                 // not need to interrogate the list.
                 //
                 return tree.pos;
+
+            case VAR_DEF:
+
+                // Ask the JFXVar for its start position and return it
+                //
+                return ((JFXVar)tree).getStartPosition();
 
             default:
 
@@ -602,6 +609,8 @@ public class JavafxTreeInfo {
             return getEndPos(((JFXTypeClass) tree).getClassName(), endPositions);
           case TIME_LITERAL:
             return tree.pos + tree.toString().length();
+          case VAR_DEF:
+            return ((JFXVar) tree).getEndPosition(endPositions);
         }
         return JavafxTreeInfo.getStartPos(tree);
     }
